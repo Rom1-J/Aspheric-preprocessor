edc8d29548f2d9926db37e24a00b1aa6cae2ade0
Ada <ada@gnous.eu>
2024-12-23 00:44:47 +0100

    feat(extractor): add extractor for email and ipv6

--- constants/fragments.go
+++ constants/fragments.go
@@ -17,8 +17,12 @@ var domainPattern = regexp.MustCompile(`([a-zA-Z0-9-.]{1,253}\.(?:` + strings.Jo
 
 var ipv4Pattern = regexp.MustCompile(`(((25[0-5]|2[0-4]\d|1\d{2}|0?\d{1,2})\b\.){3}(25[0-5]|2[0-4]\d|1\d{2}|0?\d{1,2})\b)`)
 
+var ipv6Pattern = regexp.MustCompile(`((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4})`)
+
+var emailPattern = regexp.MustCompile(`[0-9a-zA-Z-_\.+]*@([a-zA-Z0-9-.]{1,253}\.(?:` + strings.Join(TLDs, "|") + `)\b)`)
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FragmentPattern = regexp.MustCompile(strings.Join([]string{domainPattern.String(), ipv4Pattern.String()}, "|"))
+var FragmentPattern = regexp.MustCompile(strings.Join([]string{domainPattern.String(), ipv4Pattern.String(), ipv6Pattern.String(), emailPattern.String()}, "|"))
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
