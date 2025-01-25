3ab58e66cce2daef06c972bab55f4b4d70d58e30
Romain J. <romain@gnous.eu>
2025-01-25 14:14:33 +0100

    update(chunkify): add support for directory input & multithread

--- cli/chunkify.go
+++ cli/chunkify.go
@@ -2,15 +2,18 @@ package cli
 
 import (
 	"context"
-	"fmt"
-	"github.com/Rom1-J/preprocessor/constants"
-	"github.com/Rom1-J/preprocessor/logger"
 	"github.com/Rom1-J/preprocessor/process"
 	"github.com/Rom1-J/preprocessor/structs"
 	"github.com/Rom1-J/preprocessor/utils"
 	"github.com/google/uuid"
-	ucli "github.com/urfave/cli/v3"
 	"path/filepath"
+	"runtime"
+	"sync"
+
+	"github.com/Rom1-J/preprocessor/constants"
+	"github.com/Rom1-J/preprocessor/logger"
+	ucli "github.com/urfave/cli/v3"
+	"os"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,17 +29,30 @@ var Chunkify = &ucli.Command{
 			Usage:   "Verbose mode",
 			Value:   false,
 		},
-		&ucli.StringFlag{
+		&ucli.StringSliceFlag{
 			Name:     "input",
 			Aliases:  []string{"i"},
 			Usage:    "Input file",
-			Required: true,
+			Required: false,
+		},
+		&ucli.StringSliceFlag{
+			Name:     "directory",
+			Aliases:  []string{"d"},
+			Usage:    "Input directory",
+			Required: false,
 		},
 		&ucli.StringFlag{
 			Name:     "output",
 			Aliases:  []string{"o"},
 			Usage:    "Output directory",
 			Value:    "./output",
+			Required: false,
+		},
+		&ucli.IntFlag{
+			Name:     "threads",
+			Aliases:  []string{"t"},
+			Usage:    "Number of threads to use",
+			Value:    int64(runtime.NumCPU()),
 			Required: false,
 		},
 		&ucli.StringFlag{
@@ -63,67 +79,128 @@ var Chunkify = &ucli.Command{
 		logger.SetLoggerLevel(command.Bool("verbose"))
 		logger.Logger.Info().Msgf("Log level verbose: %t", command.Bool("verbose"))
 
-		inputFile := command.String("input")
-		outputDirectoryPath := filepath.Join(command.String("output"), uuid.New().String())
+		var (
+			inputList []string
 
-		metadataInfoFilePath := filepath.Join(outputDirectoryPath, "_info.csv")
-
-		logger.Logger.Info().Msgf(
-			"Chunkifing '%s' in '%s'",
-			inputFile,
-			outputDirectoryPath,
+			err error
 		)
 
-		stats, err := utils.SplitFile(
-			inputFile,
-			outputDirectoryPath,
-		)
-		if err != nil {
-			var msg = fmt.Sprintf("Error splitting file %s to %s: %v", inputFile, outputDirectoryPath, err)
-			logger.Logger.Error().Msgf(msg)
-			fmt.Println(msg)
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		//
+		// Retrieving input descriptors
+		//
+		inputFiles := command.StringSlice("input")
+		inputDirectories := command.StringSlice("directory")
 
-			return fmt.Errorf(msg)
+		inputList = append(inputList, inputFiles...)
+
+		for _, directory := range inputDirectories {
+			if err = filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if !info.IsDir() {
+					inputList = append(inputList, path)
+				}
+				return nil
+			}); err != nil {
+				return err
+			}
 		}
 
-		absoluteOutputDirectoryPath, err := filepath.Abs(outputDirectoryPath)
-		if err != nil {
-			var msg = fmt.Sprintf("Error getting absolute output path: %v", err)
-			logger.Logger.Error().Msgf(msg)
-			fmt.Println(msg)
+		logger.Logger.Debug().Msgf("Input files: %v", inputList)
 
-			return fmt.Errorf(msg)
-		}
+		logger.Logger.Info().Msgf("Chunkifing %d files", len(inputList))
+		// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-		var metadataInfo = structs.MetadataInfoStruct{
-			Name:        command.String("name"),
-			Description: command.String("description"),
-			Path:        absoluteOutputDirectoryPath,
-			Lines:       stats.Lines,
-			Parts:       stats.Parts,
-		}
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		//
+		// Chunkify files
+		//
+		var wg sync.WaitGroup
+		maxThreads := int(command.Int("threads"))
+		semaphore := make(chan struct{}, maxThreads)
 
-		if metadataInfo.Name == "" {
-			metadataInfo.Name = filepath.Base(inputFile)
-		}
+		for _, inputFile := range inputList {
+			logger.Logger.Debug().Msgf("Locking slot for: %s", inputFile)
+			semaphore <- struct{}{}
+			wg.Add(1)
 
-		metadataInfoFile, err := utils.OpenOrCreateDatabase(metadataInfoFilePath)
-		if err != nil {
-			return nil
-		}
-		if err = process.SaveMetadataInfo(metadataInfoFile, metadataInfo); err != nil {
-			return err
-		}
-		if err := metadataInfoFile.Close(); err != nil {
-			var msg = fmt.Sprintf("Error closing metadataInfo db: %v", err)
-			logger.Logger.Error().Msgf(msg)
-			fmt.Println(msg)
+			go func(filePath string) {
+				defer func() {
+					logger.Logger.Debug().Msgf("Releasing slot for: %s", filePath)
+					<-semaphore
+					wg.Done()
+				}()
 
-			return fmt.Errorf(msg)
-		}
+				// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+				//
+				// Retrieving output descriptor
+				//
+				outputDirectoryPath := filepath.Join(command.String("output"), uuid.New().String())
+				metadataInfoFilePath := filepath.Join(outputDirectoryPath, "_info.csv")
 
-		logger.Logger.Info().Msg("Processed all files")
-		fmt.Println("All files processed.")
+				absoluteOutputDirectoryPath, err := filepath.Abs(outputDirectoryPath)
+				if err != nil {
+					logger.Logger.Error().Msgf("Error getting absolute output path: %v", err)
+
+					return
+				}
+				// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+				stats, err := utils.SplitFile(
+					filePath,
+					outputDirectoryPath,
+				)
+				if err != nil {
+					logger.Logger.Error().Msgf("Error splitting file %s to %s: %v", filePath, outputDirectoryPath, err)
+
+					return
+				}
+
+				// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+				//
+				// Generating metadata
+				//
+				var metadataInfo = structs.MetadataInfoStruct{
+					Name:        command.String("name"),
+					Description: command.String("description"),
+					Path:        absoluteOutputDirectoryPath,
+					Lines:       stats.Lines,
+					Parts:       stats.Parts,
+				}
+
+				if metadataInfo.Name == "" {
+					metadataInfo.Name = filepath.Base(filePath)
+				}
+				// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+				// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+				//
+				// Saving metadata
+				//
+				metadataInfoFile, err := utils.OpenOrCreateDatabase(metadataInfoFilePath)
+				if err != nil {
+					return
+				}
+				if err = process.SaveMetadataInfo(metadataInfoFile, metadataInfo); err != nil {
+					return
+				}
+				if err := metadataInfoFile.Close(); err != nil {
+					logger.Logger.Error().Msgf("Error closing metadataInfo db: %v", err)
+
+					return
+				}
+				// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+			}(inputFile)
+		}
+		// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+		go func() {
+			wg.Wait()
+		}()
+
+		logger.Logger.Info().Msg("Done!")
 
 		return nil
 	},
